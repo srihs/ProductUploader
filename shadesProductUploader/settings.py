6382bc4de1eb888f50099e1c733b501d8557bf0d
Srimal Samarasooriya <srimalhs@gmail.com>
2019-01-15 16:33:10 +0530

    addes static root

--- shadesProductUploader/settings.py
+++ shadesProductUploader/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/' #dev
 #STATIC_URL ='/home/srimal/prod_upload/productUploader/static/'
 
